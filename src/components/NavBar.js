819103ad2e728faaebb64777062d989c2773aa1c
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-06-02 00:40:48 -0400

    menu implementation to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,44 @@
-import React  from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import React from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
 import Logo from '../assets/logo.png';
 
 const NavBar = () => {
   return (
-    <div className='fixed w-full h-[80px] flext justify-between items-center px-4 bg-[#252526] text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#252526] text-gray-300'>
       <div>
         <img src={Logo} alt='Logo' style={{ width: '60px' }} />
       </div>
+
+      {/* Menu */}
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+
+      </div>
+
+      {/*hamburger */}
+      <div className='hidden'>
+        <FaBars />
+      </div>
+
+
+      {/*mobile menu*/}
+      <ul className='hidden'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+
+      {/*Social Icons*/}
+
     </div>
   )
 }
