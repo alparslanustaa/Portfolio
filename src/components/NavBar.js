ef224d21cb878988de8f674bbe65066cb88a4979
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-06-02 00:57:51 -0400

    add functionality to hamburger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import Logo from '../assets/logo.png';
 
 const NavBar = () => {
+
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#252526] text-gray-300'>
       <div>
@@ -10,30 +14,31 @@ const NavBar = () => {
       </div>
 
       {/* Menu */}
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
-
-      </div>
-
-      {/*hamburger */}
-      <div className='hidden'>
-        <FaBars />
-      </div>
-
-
-      {/*mobile menu*/}
-      <ul className='hidden'>
+      <ul className='hidden md:flex'>
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
         <li>Work</li>
         <li>Contact</li>
+      </ul>
+
+
+
+      {/*hamburger */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        <FaBars />
+      </div>
+
+
+      {/*mobile menu*/}
+      <ul className={
+        !nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+      }>
+        <li className='py-6  text-4xl'>Home</li>
+        <li className='py-6 text-4xl'>About</li>
+        <li className='py-6 text-4xl'>Skills</li>
+        <li className='py-6 text-4xl'>Work</li>
+        <li className='py-6 text-4xl'>Contact</li>
       </ul>
 
 
