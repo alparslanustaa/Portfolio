efddb46d6270d92a9a8eb6e48508f38e958f1403
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-06-04 04:37:45 -0400

    clean code a bit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import NavBar from "./components/NavBar";
 
-
 function App() {
   return (
-    <div >
-      <NavBar/>
+    <div>
+      <NavBar />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,19 @@
-import React, { useState } from 'react'
-import { FaBars, FaTimes } from 'react-icons/fa'
-import Logo from '../assets/logo.png';
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/logo.png";
 
 const NavBar = () => {
-
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#252526] text-gray-300'>
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#252526] text-gray-300">
       <div>
-        <img src={Logo} alt='Logo' style={{ width: '60px' }} />
+        <img src={Logo} alt="Logo" style={{ width: "60px" }} />
       </div>
 
       {/* Menu */}
-      <ul className='hidden md:flex'>
+      <ul className="hidden md:flex">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
@@ -22,30 +21,29 @@ const NavBar = () => {
         <li>Contact</li>
       </ul>
 
-
-
       {/*hamburger */}
-      <div onClick={handleClick} className='md:hidden z-10'>
+      <div onClick={handleClick} className="md:hidden z-10">
         <FaBars />
       </div>
 
-
       {/*mobile menu*/}
-      <ul className={
-        !nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
-      }>
-        <li className='py-6  text-4xl'>Home</li>
-        <li className='py-6 text-4xl'>About</li>
-        <li className='py-6 text-4xl'>Skills</li>
-        <li className='py-6 text-4xl'>Work</li>
-        <li className='py-6 text-4xl'>Contact</li>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6  text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
       </ul>
 
- 
       {/*Social Icons*/}
-
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
