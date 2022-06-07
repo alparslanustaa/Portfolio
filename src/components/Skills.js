29dff304475fbbfa95cb67c5f96095ea467b96d4
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-06-07 01:48:34 -0400

    container element create for skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,23 @@
 import React from "react";
 
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import JavaScript from "../assets/javascript.png";
+import ReactImg from "../assets/react.png";
+import Node from "../assets/node.png";
+import GitHub from "../assets/github.png";
+import Tailwind from "../assets/tailwind.png";
+
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div className="skills">
+      {/*container*/}
+      <div>
+        <p>Experience</p>
+        <p>Techologies I have familier with</p>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
