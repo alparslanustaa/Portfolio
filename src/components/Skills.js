5f56f368d9b9f71e9c18130d41ad060967eed0de
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-06-07 02:18:22 -0400

    import all the icons and css them

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,20 +7,53 @@ import ReactImg from "../assets/react.png";
 import Node from "../assets/node.png";
 import GitHub from "../assets/github.png";
 import Tailwind from "../assets/tailwind.png";
+import Psql from "../assets/psql.png";
 
 const Skills = () => {
   return (
-    <div name="skills" className="bg-[#0a192f] text-gray-300">
-      {/*container*/}
-      <div>
-        <p>Experience</p>
-        <p>Techologies I have familier with</p>
-      </div>
-
-      <div>
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
-          <img src={HTML} alt="HTML icon" />
-          <p>HTML</p>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
+            Skills
+          </p>
+          <p className="py-4">These are the technologies I am working with</p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
+            <p className="my-4">HTML</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={CSS} alt="HTML icon" />
+            <p className="my-4">CSS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={JavaScript} alt="HTML icon" />
+            <p className="my-4">JavaScript</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
+            <p className="my-4">React</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Node} alt="HTML icon" />
+            <p className="my-4">Node</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={GitHub} alt="HTML icon" />
+            <p className="my-4">GitHub</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Psql} alt="HTML icon" />
+            <p className="my-4">Psql</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
+            <p className="my-4">Tailwind</p>
+          </div>
         </div>
       </div>
     </div>
