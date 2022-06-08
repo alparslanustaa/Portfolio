e64384460125248e1c53e87367c2b553150535cd
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-06-08 03:13:37 -0400

    projects section implementation

---
+++ src/components/Projects.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div
+      name="projects"
+      className="w-full md:h-screen text-gray-300 bg-[#0a192f]"
+    >
+      Projects
+      <div>
+        <div>
+          <p>Projects</p>
+          <p>Here is some of my recent projects</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
