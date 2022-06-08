c297a7799d77b7d029b7c4456b1e40f258c3cc32
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-06-08 03:52:21 -0400

    style containers and project sections

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,11 +6,12 @@ const Projects = () => {
       name="projects"
       className="w-full md:h-screen text-gray-300 bg-[#0a192f]"
     >
-      Projects
-      <div>
-        <div>
-          <p>Projects</p>
-          <p>Here is some of my recent projects</p>
+      <div className="max-w[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Projects
+          </p>
+          <p className="py-6">Here is some of my recent projects</p>
         </div>
         <div>
           <div>
