17c5e9be8520382332e65327bd8434722153881c
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-06-10 01:57:55 -0400

    mobile menu functionality fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
     >
       <form
         method="POST"
-        action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c"
+        action="https://getform.io/f/9c35f77b-7718-4cf9-b074-a6f8c388fbc4"
         className="flex flex-col max-w-[600px] w-full"
       >
         <div className="pb-8">
